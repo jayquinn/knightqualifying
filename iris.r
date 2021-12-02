d5838f60cf099e82233592230051bbf8a26c343a
jayquinn <61223949+jayquinn@users.noreply.github.com>
2021-12-02 23:45:51 +0900

    asdsdas

--- iris.r
+++ iris.r
@@ -8,3 +8,11 @@ X_train = ds[idx,][-5]
 y_train = ds[idx,][5]
 X_test = ds[-idx,][-5]
 y_test = ds[-idx,][5]
+
+train = cbind(X_train,y_train) 
+test = cbind(X_test,y_test)
+
+mod = randomForest(Species ~., data = train)
+answer = predict(mod, newdata=test[-5])
+
+confusionMatrix(y_test$Species,answer)
